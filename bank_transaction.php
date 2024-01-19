f575ff47cd2a3a617000626d64caacc7d9294880
Catherine Lorraine Odon <cathlorrodon.pup@gmail.com>
2024-01-19 23:24:07 +0800

    UPDATED TRANSACTION FILES

--- bank_transaction.php
+++ bank_transaction.php
@@ -1,24 +1,27 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $date = $_POST["date"];
-    $description = $_POST["description"];
-    $amount = $_POST["amount"];
+    $description = $_POST["description"] ?? "";
+    $amount = $_POST["amount"] ?? 0;
+    
+
+    // Get the current date and time
+    $date = date("Y-m-d H:i:s");
+
 
     $servername = "127.0.0.1";
     $username = "root";
     $password = "";
-    $dbname = "database_bankfinance";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $database = "database_bankfinance";
+    
+    $conn = new mysqli($servername, $username, $password, $database);
+    
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $accountNumber = generateUniqueAccountNumber($conn);
-    // SQL query to insert user data into the 'transaction_history' table 
-    $sql = "INSERT INTO transaction_history (date, description, amount, accountNumber) 
-            VALUES ('$date', '$description', '$amount','$accountNumber')";
+    $sql = "INSERT INTO transaction_history (date, description, amount) 
+            VALUES ('$date', '$description', $amount)";
 
     if ($conn->query($sql) === TRUE) {
         echo "Transaction saved successfully!";
@@ -27,16 +30,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     $conn->close();
-}
-
-function generateUniqueAccountNumber($conn) {
-    // Customize the format or length of the account number as needed
-    do {
-        $accountNumber = rand(1000000000, 9999999999);
-        $query = "SELECT * FROM transaction_history WHERE accountNumber = '$accountNumber'";
-        $result = $conn->query($query);
-    } while ($result->num_rows > 0);
-
-    return $accountNumber;
 }
 ?>
